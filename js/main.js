3e2b12b29317115d39d304ff1918308b0892eb39
diana <diana@diana-HP-Pavilion-15-Notebook-PC>
2018-11-13 21:44:21 +0000

    AI first layer ready, #1

--- js/main.js
+++ js/main.js
@@ -6,10 +6,46 @@
 
 //$('#one').text('Other staff');
 
-
+var button;
 $(document).ready(function () {
     $('.td').click(function (e) {
-        $(this).toggleClass('choosen');
-      $('#temp').text(this.id);
+
+        $('#temp').text(this.id + ' ' + ai(this));
+
     });
 });
+
+function ai(b) {
+    if (button) {
+        if (button.id === b.id) {
+            $(button).removeClass('choosen');
+            button = null;
+            return 'the same';
+
+        }
+        ;
+
+        var text = '#' + button.id;
+        //text+=b.id;
+        var img1 = $(b).children(':first').attr('src');
+        var img2 = $(button).children(':first').attr('src');
+        $(button, b).removeClass('choosen');
+        button = null;
+
+        if (img1 === img2) {
+       //check for reachability
+       if (reachability()){
+            return 'yes';
+        }
+        else {
+            return 'not reachable';
+        }
+        }
+        return 'not at all';
+    } else {
+
+        button = b;
+        $(button).addClass('choosen');
+        return 'only one';
+    }
+}
